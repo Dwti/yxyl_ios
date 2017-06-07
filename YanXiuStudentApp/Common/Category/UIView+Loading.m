6cab182283733b20d0ab07e1cad11423e9b9e564
niuzhaowang <niuzhaowang.yanxiu.com>
2017-06-07 16:49:34 +0800

    submit

--- YanXiuStudentApp/Common/Category/UIView+Loading.m
+++ YanXiuStudentApp/Common/Category/UIView+Loading.m
@@ -7,68 +7,31 @@
 //
 
 #import "UIView+Loading.h"
-#import "YXLoadingView.h"
-#import "YXToastView.h"
-#import "YXTipsView.h"
-
-static YXToastView *_toastView;
-static YXTipsView *_tipsView;
+#import <MBProgressHUD/MBProgressHUD.h>
 
 @implementation UIView (Loading)
 
 - (void)nyx_startLoading {
     [self endEditing:YES];
     [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
-    [YXLoadingControl startLoadingWithSuperview:self];
+    if ([MBProgressHUD HUDForView:self]) {
+        return;
+    }
+    [MBProgressHUD showHUDAddedTo:self animated:YES];
 }
 
 - (void)nyx_stopLoading {
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
-    [YXLoadingControl stopLoadingWithSuperview:self];
+    [MBProgressHUD hideHUDForView:self animated:YES];
 }
 
 - (void)nyx_showToast:(NSString *)text {
-    if (![text yx_isValidString]) {
-        return;
-    }
-    if (!_toastView) {
-        _toastView = [[YXToastView alloc] init];
-    }
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-        [self addSubview:_toastView];
-    } else {
-        [[UIApplication sharedApplication].keyWindow addSubview:_toastView];
-    }
-    [_toastView mas_remakeConstraints:^(MASConstraintMaker *make) {
-        make.edges.mas_equalTo(0);
-    }];
-    _toastView.text = text;
-    [_toastView show:YES];
-    [_toastView hide:YES afterDelay:1.f];
+    [MBProgressHUD hideAllHUDsForView:self animated:NO];
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self animated:YES];
+    hud.mode = MBProgressHUDModeText;
+    hud.labelText = text;
+    hud.removeFromSuperViewOnHide = YES;
+    [hud hide:YES afterDelay:1];
 }
 
-- (void)nyx_showTipsWithTitle:(NSString *)title
-                        text:(NSString *)text
-                  detailText:(NSString *)detailText
-{
-    if (![title yx_isValidString] || ![text yx_isValidString]) {
-        return;
-    }
-    if (!_tipsView) {
-        _tipsView = [[YXTipsView alloc] init];
-    }
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-        [self addSubview:_tipsView];
-    } else {
-        [[UIApplication sharedApplication].keyWindow addSubview:_tipsView];
-    }
-    [_tipsView mas_remakeConstraints:^(MASConstraintMaker *make) {
-        make.edges.mas_equalTo(0);
-    }];
-    _tipsView.title = title;
-    _tipsView.text = text;
-    _tipsView.detailText = detailText;
-    [_tipsView show:YES];
-    [_tipsView hide:YES afterDelay:1.f];
-}
 @end
